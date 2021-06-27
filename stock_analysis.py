79023549eae50c0069a3ceec7034d7eb06af1122
Eric <eyem0002@student.monsah.edu>
2021-06-27 20:49:38 +1000

    scraping CBA data and saving as pickle

--- stock_analysis.py
+++ stock_analysis.py
@@ -1,1 +1,22 @@
-#seeing if the push works
+from typing import Type
+from pandas_datareader import data as pd
+import pandas
+import datetime 
+
+# taking start and end dates from today to two years ago
+start = datetime.datetime.now() - datetime.timedelta(days=2*365)
+end = datetime.datetime.now()
+# reading the data from two years ago to today for a particular stock
+raw_data = pd.DataReader('CBA', 'yahoo', start, end)
+
+# pickling the data frame so do not have reload every time
+raw_data.to_pickle('CBA_frame.pickle')
+stock_frame = pandas.read_pickle('CBA_frame.pickle')
+print(stock_frame.head())
+
+
+
+
+
+
+
