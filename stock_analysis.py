8a0c21d7e9a37057a06ca61dd4bb2d0c423640c3
Eric <eyem0002@student.monsah.edu>
2021-06-27 20:59:24 +1000

    using sys

--- stock_analysis.py
+++ stock_analysis.py
@@ -2,8 +2,13 @@ from typing import Type
 from pandas_datareader import data as pd
 import pandas
 import datetime 
+import sys
 
-# taking start and end dates from today to two years ago
+
+# start date, ticker 
+# taking start date from today to how many years ago
+def required_date(datetime: start, datetime: end):
+    
 start = datetime.datetime.now() - datetime.timedelta(days=2*365)
 end = datetime.datetime.now()
 # reading the data from two years ago to today for a particular stock
@@ -15,7 +20,10 @@ stock_frame = pandas.read_pickle('CBA_frame.pickle')
 print(stock_frame.head())
 
 
-
+if __name__ == "__main__":
+    if len(sys.argv) == 3 or len(sys.argv) == 4:
+        
+        
 
 
 
